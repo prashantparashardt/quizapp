9a6f282a752767f804d290c5b2c098e3cab8d7d5
PRASHANT PARASHAR (CRGL-THIRDPARTY.COM) <prashant_parashar@crgl-thirdparty.com>
2021-05-13 01:33:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
+          Edt
           <code>src/App.js</code>
           and save to reload.
         </p>
