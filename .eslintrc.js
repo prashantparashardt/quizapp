bf5b378e73bcd05ff3fcea31c4da1f6f653ca69e
PRASHANT PARASHAR (CRGL-THIRDPARTY.COM) <prashant_parashar@crgl-thirdparty.com>
2021-05-13 01:30:15 +0530

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,11 @@
 // }
 
 module.exports = {
-  extends: ['airbnb'],
+  extends: ['airbnb', 'prettier'],
   env: {
     browser: true,
     node: true,
+    jest: true,
   },
   rules: {
     'react/jsx-filename-extension': [
